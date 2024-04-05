f4a8448f827b3d571c414ed92a2f1ad1befe6e40
Ahsan <ahsanaz461@gmail.com>
2024-04-05 23:48:27 +0700

    feat: Fetch data from a MongoDB database

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,21 @@
+import prisma from '$lib/prisma';
+import type { PageServerLoad } from './$types';
+
+export const load = (async () => {
+	const fullName = await prisma.content.findFirst({ where: { key: 'fullName' } });
+	const username = await prisma.content.findFirst({ where: { key: 'username' } });
+	const bio = await prisma.content.findFirst({ where: { key: 'bio' } });
+	const picture = await prisma.content.findFirst({ where: { key: 'picture' } });
+
+	const links = await prisma.link.findMany();
+
+	return {
+		profile: {
+			username: username?.value,
+			fullName: fullName?.value,
+			bio: bio?.value,
+			picture: picture?.value
+		},
+		links
+	};
+}) satisfies PageServerLoad;
