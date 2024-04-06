751b5235f4b19a8a27b50cc8e7d26b53977de58f
Ahsan <ahsanaz461@gmail.com>
2024-04-06 18:43:30 +0700

    refactor: Make a content value getter

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,20 +1,25 @@
 import prisma from '$lib/prisma';
 import type { PageServerLoad } from './$types';
 
+const getContentValue = async (key: string) => {
+	const content = await prisma.content.findFirst({ where: { key } });
+	return content?.value;
+};
+
 export const load = (async () => {
-	const fullName = await prisma.content.findFirst({ where: { key: 'fullName' } });
-	const username = await prisma.content.findFirst({ where: { key: 'username' } });
-	const bio = await prisma.content.findFirst({ where: { key: 'bio' } });
-	const picture = await prisma.content.findFirst({ where: { key: 'picture' } });
+	const fullName = await getContentValue('fullName');
+	const username = await getContentValue('username');
+	const bio = await getContentValue('bio');
+	const picture = await getContentValue('picture');
 
 	const links = await prisma.link.findMany({});
 
 	return {
 		profile: {
-			username: username?.value,
-			fullName: fullName?.value,
-			bio: bio?.value,
-			picture: picture?.value
+			username: username,
+			fullName: fullName,
+			bio: bio,
+			picture: picture
 		},
 		links
 	};
